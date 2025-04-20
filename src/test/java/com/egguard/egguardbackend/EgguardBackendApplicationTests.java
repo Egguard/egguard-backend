180ceb4569457cc4245ad1edf27a3e9ea3dd8158
JuanDiaz <juanben10xlr99@gmail.com>
2025-04-20 12:01:38 +0100

    chore: project initialized

---
+++ src/test/java/com/egguard/egguardbackend/EgguardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egguard.egguardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgguardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
